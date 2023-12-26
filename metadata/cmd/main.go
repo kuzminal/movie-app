4342f77a51b354c4954ce4985854b20ab2be33a6
Кузьмин Алексей <aleschakuzm1n@yandex.ru>
2023-12-26 10:48:25 +0300

    Сервис метаданных

---
+++ metadata/cmd/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"metadata/internal/controller/metadata"
+	httphandler "metadata/internal/handler/http"
+	"metadata/internal/repository/memory"
+)
+
+func main() {
+	log.Println("Starting the movie metadata service")
+	repo := memory.New()
+	ctrl := metadata.New(repo)
+	h := httphandler.New(ctrl)
+	http.Handle("/metadata", http.HandlerFunc(h.GetMetadata))
+	if err := http.ListenAndServe(":8081", nil); err != nil {
+		panic(err)
+	}
+}
